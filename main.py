81c0bac887d36087a3794f3ffe142769b039e35c
Ricardo Dematte <103331571+ricardodematte@users.noreply.github.com>
2022-05-02 19:19:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Multiplicação de 3 números reais dividido por 2
+
+import os
+n1 = (int(input("Digite um número inteiro n1:\n")))
+n2 = (int(input("Digite um número inteiro n2:\n")))
+n3 = (int(input("Digite um número inteiro n3:\n")))
+mult = n1*n2*n3
+os.system("clear")
+print ("A multiplicação é ",mult)
